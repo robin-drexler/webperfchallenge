1138c916cafa9168faf838effa887e9cf740a25b
Robin Drexler <drexler.robin@gmail.com>
2016-11-06 21:43:50 +0100

    feat: prototype

--- src/App.js
+++ src/App.js
@@ -1,21 +1,153 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component, PropTypes} from 'react';
 import './App.css';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import TextField from 'material-ui/TextField';
+import AppBar from 'material-ui/AppBar';
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import {Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn} from 'material-ui/Table';
+
+
+injectTapEventPlugin();
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    state = {
+        topsites: [],
+        loading: false,
+        result: {},
+        error: null,
+    };
+
+    componentDidMount() {
+        const topsitesRef = this.props.firebase.database().ref("topsites").orderByChild("desktop");
+
+        topsitesRef.on('value', (snapshot) => {
+            const topsites = snapshot.val().sort(function (a, b) {
+                const keyA = a.mobile;
+                const keyB = b.mobile;
+
+                if (keyA < keyB) return 1;
+                if (keyA > keyB) return -1;
+                return 0;
+            });
+            this.setState({
+                topsites: topsites,
+            });
+        });
+    }
+
+    executePageSpeed = (e) => {
+        e.preventDefault();
+        const url = encodeURI(this.value);
+
+        this.setState({
+            loading: true,
+            error: false,
+        });
+
+        fetch(`https://www.googleapis.com/pagespeedonline/v2/runPagespeed?url=${url}&strategy=mobile`)
+            .then(r => r.json())
+            .then((json) => {
+                if (json.error) {
+                    throw new Error(json.error.message)
+                }
+                const score = json.ruleGroups.SPEED.score;
+                const betterPerformingPages = this.state.topsites.filter((site) => site.mobile > score);
+
+                this.setState({
+                    loading: false,
+                    result: {
+                        speed: score,
+                        betterPages: betterPerformingPages,
+                    }
+                });
+            }).catch((err) => {
+                this.setState({
+                    loading: false,
+                    error: err.message,
+                });
+            });
+    };
+
+    renderBody() {
+        if (typeof this.state.result.speed === 'undefined') {
+            return (<form className="form" key="form" onSubmit={this.executePageSpeed}>
+                <TextField
+                    className="url-input"
+                    id="search"
+                    errorText={this.state.error}
+                    floatingLabelText="Enter your website URL"
+                    onChange={(_, value) => this.value =  value}
+                    disabled={this.state.loading}
+                />
+                <RaisedButton disabled={this.state.loading} label="go!" onClick={this.executePageSpeed}/>
+            </form>)
+        } else {
+            let betterPagesPercentage = 100 / this.state.topsites.length * this.state.result.betterPages.length;
+            betterPagesPercentage = Math.ceil(betterPagesPercentage);
+            return (
+                <div className="result" key="result">
+                    <div className="result-description">
+                        <Card>
+                            <CardHeader title="Result" />
+                            <CardText>
+                                {betterPagesPercentage}% of top 50 websites perform better than you!
+                            </CardText>
+                        </Card>
+                    </div>
+                    <div className="result-table">
+                        <Card>
+                            <Table allRowsSelected={false} selectable={false} className="overview">
+                                <TableHeader adjustForCheckbox={false} displaySelectAll={false}>
+                                    <TableRow>
+                                        <TableHeaderColumn>site</TableHeaderColumn>
+                                        <TableHeaderColumn>mobile score</TableHeaderColumn>
+                                    </TableRow>
+                                </TableHeader>
+                                <TableBody displayRowCheckbox={false}>
+                                    {this.state.result.betterPages.map((entry, idx) => {
+                                        return (
+                                            <TableRow key={idx}>
+                                                <TableRowColumn>{entry.site}</TableRowColumn>
+                                                <TableRowColumn>{entry.mobile}</TableRowColumn>
+                                            </TableRow>
+                                        );
+                                    })}
+                                </TableBody>
+                            </Table>
+                        </Card>
+                    </div>
+                </div>
+            );
+        }
+    }
+
+    render() {
+        return (
+            <MuiThemeProvider>
+                <div className="App">
+                    <AppBar iconElementLeft={<span/>} title="How slow is your site on mobile?" />
+                    <div className="App-intro">
+                        <ReactCSSTransitionGroup
+                            transitionName="score"
+                            transitionEnterTimeout={500}
+                            transitionLeaveTimeout={500}
+                        >
+                            {this.renderBody()}
+                        </ReactCSSTransitionGroup>
+
+                    </div>
+                </div>
+            </MuiThemeProvider>
+        );
+    }
 }
+
+App.propTypes = {
+    firebase: PropTypes.object.isRequired
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,8 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import firebase from 'firebase';
+
+const config = {
+    apiKey: "AIzaSyDJTcHWR87skx2hnXaqku50ZO0LKYEGpc4",
+    authDomain: "pagespeed-51d5f.firebaseapp.com",
+    databaseURL: "https://pagespeed-51d5f.firebaseio.com",
+    storageBucket: "pagespeed-51d5f.appspot.com",
+    messagingSenderId: "615133544374"
+};
+firebase.initializeApp(config);
 
 ReactDOM.render(
-  <App />,
+  <App firebase={firebase} />,
   document.getElementById('root')
 );
