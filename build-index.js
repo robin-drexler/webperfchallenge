c316bb8421f8761fa9156b81435dd1a3b356c3ea
Robin Drexler <drexler.robin@gmail.com>
2016-11-07 13:33:12 +0100

    remove impressum from bottom

--- build-index.js
+++ build-index.js
@@ -98,11 +98,6 @@ const start = `<!doctype html>
     <div id="root">`;
 
 const end = `</div>
-
-<div class="footer">
-    <a href="/about.html">Impressum</a>
-</div>
-
 <script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
