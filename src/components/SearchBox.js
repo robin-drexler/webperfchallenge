20a61ba7ef6e3a48ec0a73a2880e948d5905d416
Robin Drexler <drexler.robin@gmail.com>
2016-11-20 16:42:24 +0100

    Make SearchBox funcitonal

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,49 +1,46 @@
-import React, { Component, PropTypes } from 'react';
+import React, {PropTypes} from 'react';
 import TextField from 'material-ui/TextField';
 import {Card, CardText} from 'material-ui/Card';
 import RaisedButton from 'material-ui/RaisedButton';
 
 
-export default class SearchBox extends Component {
+export default function SearchBox(props) {
 
-    handleSubmit = (e) => {
+    const handleSubmit = (e) => {
         e.preventDefault();
-        this.props.onSearch();
+        props.onSearch();
     };
+    const buttonText = props.loading ? 'loading...' : 'analyze';
 
-    render() {
-        const buttonText = this.props.loading ? 'loading...' : 'analyze';
+    return (
+        <form className="form" key="form" onSubmit={handleSubmit}>
 
-        return (
-            <form className="form" key="form" onSubmit={this.handleSubmit}>
-
-                <div className="card">
-                    <Card>
-                        <CardText>
-                            <h2>
-                                Compare your website's mobile performance with the top 50 most popular in the world.
-                            </h2>
-                            <div className="input-content">
-                                <TextField
-                                    className="url-input"
-                                    id="search"
-                                    errorText={this.props.error}
-                                    floatingLabelText="Enter your website URL"
-                                    disabled={this.props.loading}
-                                    value={this.props.searchValue}
-                                    onChange={this.props.onSearchInputChange}
-                                />
-                                <div>
-                                    <RaisedButton primary disabled={this.props.loading} label={buttonText}
-                                                  onClick={this.handleSubmit}/>
-                                </div>
+            <div className="card">
+                <Card>
+                    <CardText>
+                        <h2>
+                            Compare your website's mobile performance with the top 50 most popular in the world.
+                        </h2>
+                        <div className="input-content">
+                            <TextField
+                                className="url-input"
+                                id="search"
+                                errorText={props.error}
+                                floatingLabelText="Enter your website URL"
+                                disabled={props.loading}
+                                value={props.searchValue}
+                                onChange={props.onSearchInputChange}
+                            />
+                            <div>
+                                <RaisedButton primary disabled={props.loading} label={buttonText}
+                                              onClick={handleSubmit}/>
                             </div>
-                        </CardText>
-                    </Card>
-                </div>
-            </form>
-        );
-    }
+                        </div>
+                    </CardText>
+                </Card>
+            </div>
+        </form>
+    );
 }
 
 SearchBox.propTypes = {
